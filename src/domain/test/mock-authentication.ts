eb6e2d037c4b272cf6721eaf48b78bcf783d4ba9
Bruno Rezende <brunoc.rez@gmail.com>
2023-03-03 11:04:47 -0500

    test: allowing RemoteAuth to receive body

---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,7 @@
+import { AuthenticationParams } from 'domain/usecases/authentication'
+import faker from 'faker'
+
+export const mockAuth = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
