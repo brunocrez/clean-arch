e4ece929f71b0343b7f30690518da08e765191a4
Bruno Rezende <brunoc.rez@gmail.com>
2023-03-03 10:30:04 -0500

    chore: adding faker and using it on tests

--- src/data/usecases/authentication/remote-authentication.test.ts
+++ src/data/usecases/authentication/remote-authentication.test.ts
@@ -1,12 +1,13 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClient: HttpPostClientSpy
 }
 
-const makeSut = (url = 'MOCK_URL'): SutTypes => {
+const makeSut = (url = faker.internet.url()): SutTypes => {
   const httpPostClient = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClient)
   return { sut, httpPostClient }
@@ -14,7 +15,7 @@ const makeSut = (url = 'MOCK_URL'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   test('should call http client with correct URL', async () => {
-    const url = 'DEFAULT_URL'
+    const url = faker.internet.url()
     const { sut, httpPostClient } = makeSut(url)
     await sut.auth()
     expect(httpPostClient.url).toBe(url)
