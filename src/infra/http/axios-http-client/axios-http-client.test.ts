8346b88b580c711b269b174370463b6e5a5f11a8
Bruno Rezende <brunoc.rez@gmail.com>
2023-03-04 20:03:29 -0500

    feat: creating axios http client + unit tests

---
+++ src/infra/http/axios-http-client/axios-http-client.test.ts
@@ -0,0 +1,16 @@
+import { AxiosHttpClient } from './axios-http-client'
+
+import axios from 'axios'
+import faker from 'faker'
+
+jest.mock('axios')
+const mockAxios = axios as jest.Mocked<typeof axios>
+
+describe('AxiosHttpClient', () => {
+  test('should call axios with right URL', async () => {
+    const url = faker.internet.url()
+    const sut = new AxiosHttpClient()
+    await sut.post({ url })
+    expect(mockAxios).toHaveBeenCalledWith(url)
+  })
+})
