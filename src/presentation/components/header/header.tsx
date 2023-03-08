6da20667b66596dcfe58e74eac9c2b2186ec913a
Bruno Rezende <brunoc.rez@gmail.com>
2023-03-08 08:33:32 -0500

    refactor: creating header and footer components

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React, { memo } from 'react'
+import styles from './header-styles.scss'
+
+import Logo from '@/presentation/components/logo/logo'
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <h1>4Dev - Enquete para Programadores</h1>
+    </header>
+  )
+}
+
+export default memo(Header)
