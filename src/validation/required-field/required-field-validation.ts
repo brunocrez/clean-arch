c78994cad22109ddd120ab6b2bc96d90073d5b9a
Bruno Rezende <brunoc.rez@gmail.com>
2023-03-13 17:51:05 -0500

    feat: adding validation for empty required fields

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,9 @@
+import { RequiredFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor(readonly name: string) {}
+  validate(value: string): Error {
+    return new RequiredFieldError()
+  }
+}
